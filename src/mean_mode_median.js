f86df188e447b2d0d04f866a7ff8b2157a6f8f5c
sunwookim <sunkim09@g.skku.edu>
2023-01-01 16:09:55 +0900

    ADD src/mean_mode_median file

---
+++ src/mean_mode_median.js
@@ -0,0 +1,13 @@
+const mean = (arr) => {
+    var answer = 0;
+
+    let sum = 0;
+    for(let i=0; i<arr.length; ++i){
+        sum += arr[i];
+    }
+
+    answer = sum / arr.length;
+    return answer;
+}
+
+const mode
