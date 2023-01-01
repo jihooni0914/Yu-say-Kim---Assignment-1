c948e31b769139e126f32ee3c897bd8c8e7565ab
sunwookim <sunkim09@g.skku.edu>
2023-01-01 16:37:25 +0900

    ADD mode, median

--- src/mean_mode_median.js
+++ src/mean_mode_median.js
@@ -1,4 +1,4 @@
-const mean = (arr) => {
+function mean(arr) {
     var answer = 0;
 
     let sum = 0;
@@ -10,4 +10,42 @@ const mean = (arr) => {
     return answer;
 }
 
-const mode
+function mode(arr) {
+    var answer = 0;
+
+    var cnt = {};
+
+    arr.forEach((e) => {
+        if (cnt[e]) {
+            cnt[e] += 1;
+        } else {
+            cnt[e] = 1;
+        }
+    });
+
+    var mode = cnt[Object.keys(cnt)[0]];
+    answer = Object.keys(cnt)[0];
+
+    for (let i in cnt) {
+        if (cnt[i] > mode) {
+            mode = cnt[i];
+            answer = Object.keys(cnt)[i];
+        }
+    }
+
+    return answer;
+}
+
+function median(arr){
+    var answer = 0;
+    arr.sort();
+    var center = parseInt(arr.length / 2);
+
+    if(arr.length % 2 == 1){
+        answer = arr[center];
+    } else {
+        answer = (arr[center-1] + arr[center])/2;
+    }
+
+    return answer;
+}
