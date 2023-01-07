d995273575fb880031d423fbfc4e7a4934a77e0f
jihooni0914 <dbwlgns8314@gmail.com>
2023-01-07 14:47:41 +0900

    Add newline to echo

--- tools/post-generate-lyric.sh
+++ tools/post-generate-lyric.sh
@@ -2,46 +2,46 @@
 
 FILE=oort-cloud.md
 
-echo "Let's go" >> $FILE
+echo -e "Let's go\n" >> $FILE
 git add $FILE
 git commit -m "Add 11th sentence"
 
-echo "새로운 길의 탐험가" >> $FILE
+echo -e "새로운 길의 탐험가\n" >> $FILE
 git add $FILE
 git commit -m "Add 12th sentence"
 
-echo "Beyond the road" >> $FILE
+echo -e "Beyond the road\n" >> $FILE
 git add $FILE
 git commit -m "Add 13th sentence"
 
-echo "껍질을 깨뜨려버리자" >> $FILE
+echo -e "껍질을 깨뜨려버리자\n" >> $FILE
 git add $FILE
 git commit -m "Add 14th sentence"
 
-echo "두려움은 이제 거둬" >> $FILE
+echo -e "두려움은 이제 거둬\n" >> $FILE
 git add $FILE
 git commit -m "Add 15th sentence"
 
-echo "오로지 나를 믿어" >> $FILE
+echo -e "오로지 나를 믿어\n" >> $FILE
 git add $FILE
 git commit -m "Add 16th sentence"
 
-echo "지금이 바로 Time to fly" >> $FILE
+echo -e "지금이 바로 Time to fly\n" >> $FILE
 git add $FILE
 git commit -m "Add 17th sentence"
 
-echo "두 눈 앞의 끝 사뿐 넘어가" >> $FILE
+echo -e "두 눈 앞의 끝 사뿐 넘어가\n" >> $FILE
 git add $FILE
 git commit -m "Add 18th sentence"
 
-echo "한계 밖의 Trip 짜릿하잖아" >> $FILE
+echo -e "한계 밖의 Trip 짜릿하잖아\n" >> $FILE
 git add $FILE
 git commit -m "Add 19th sentence"
 
-echo "녹이 슨 심장에 쉼 없이 피는 꿈" >> $FILE
+echo -e "녹이 슨 심장에 쉼 없이 피는 꿈\n" >> $FILE
 git add $FILE
 git commit -m "Add 20th sentence"
 
-echo "무모하대도 믿어 난" >> $FILE
+echo -e "무모하대도 믿어 난\n" >> $FILE
 git add $FILE
 git commit -m "Add 21th sentence"
