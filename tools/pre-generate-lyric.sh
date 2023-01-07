53f901836e14025104395fe2d6c93dcb52c2eb6b
sunwookim <sunkim09@g.skku.edu>
2023-01-07 14:46:58 +0900

    FEAT add \n

--- tools/pre-generate-lyric.sh
+++ tools/pre-generate-lyric.sh
@@ -20,46 +20,46 @@ echo "# 오르트 구름 (윤하)" >> $FILE
 
 git add $FILE && git commit -m "Title"
 
-echo "![Oort cloud cover](./cover_picture.jpg)" >> $FILE
+echo -e "![Oort cloud cover](./cover_picture.jpg)\n" >> $FILE
 
 git add $FILE && git commit -m "Cover picture"
 
-echo "어둠만이 나의 전부였던 동안" >> $FILE
+echo -e "어둠만이 나의 전부였던 동안\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 1줄"
 
-echo "숨이 벅차도록 달려왔잖아" >> $FILE
+echo -e "숨이 벅차도록 달려왔잖아\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 2줄"
 
-echo "Never say time's up" >> $FILE
+echo -e "Never say time's up\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 3줄"
 
-echo "경계의 끝자락" >> $FILE
+echo -e "경계의 끝자락\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 4줄"
 
-echo "내 끝은 아니니까" >> $FILE
+echo -e "내 끝은 아니니까\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 5줄"
 
-echo "울타리 밖에 일렁이는 무언가" >> $FILE
+echo -e "울타리 밖에 일렁이는 무언가\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 6줄"
 
-echo "그 아무도 모르는 별일지 몰라" >> $FILE
+echo -e "그 아무도 모르는 별일지 몰라\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 7줄"
 
-echo "I wanna wanna be there" >> $FILE
+echo -e "I wanna wanna be there\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 8줄"
 
-echo "I'm gonna gonna be there" >> $FILE
+echo -e "I'm gonna gonna be there\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 9줄"
 
-echo "벅찬 맘으로 이 궤도를 벗어나" >> $FILE
+echo -e "벅찬 맘으로 이 궤도를 벗어나\n" >> $FILE
 
 git add $FILE && git commit -m "ADD 오르트 구름 10줄"
