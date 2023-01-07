0774bb48c2bbbfc74b157b5db9083b2c765c9a01
jihooni0914 <dbwlgns8314@gmail.com>
2023-01-07 14:31:01 +0900

    Change hard-coded file name to $File

--- tools/post-generate-lyric.sh
+++ tools/post-generate-lyric.sh
@@ -3,45 +3,45 @@
 FILE=oort-cloud.md
 
 echo "Let's go"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 11th sentence"
 
 echo "새로운 길의 탐험가"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 12th sentence"
 
 echo "Beyond the road"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 13th sentence"
 
 echo "껍질을 깨뜨려버리자"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 14th sentence"
 
 echo "두려움은 이제 거둬"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 15th sentence"
 
 echo "오로지 나를 믿어"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 16th sentence"
 
 echo "지금이 바로 Time to fly"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 17th sentence"
 
 echo "두 눈 앞의 끝 사뿐 넘어가"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 18th sentence"
 
 echo "한계 밖의 Trip 짜릿하잖아"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 19th sentence"
 
 echo "녹이 슨 심장에 쉼 없이 피는 꿈"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 20th sentence"
 
 echo "무모하대도 믿어 난"
-git add oort-cloud.md
+git add $FILE
 git commit -m "Add 21th sentence"
