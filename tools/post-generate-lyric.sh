37eb46e4027b99323ceebdddc09d3b9bde8f326a
jihooni0914 <dbwlgns8314@gmail.com>
2023-01-07 14:44:36 +0900

    Fix invalid syntax of echo

--- tools/post-generate-lyric.sh
+++ tools/post-generate-lyric.sh
@@ -2,46 +2,46 @@
 
 FILE=oort-cloud.md
 
-echo "Let's go"
+echo "Let's go" >> $FILE
 git add $FILE
 git commit -m "Add 11th sentence"
 
-echo "새로운 길의 탐험가"
+echo "새로운 길의 탐험가" >> $FILE
 git add $FILE
 git commit -m "Add 12th sentence"
 
-echo "Beyond the road"
+echo "Beyond the road" >> $FILE
 git add $FILE
 git commit -m "Add 13th sentence"
 
-echo "껍질을 깨뜨려버리자"
+echo "껍질을 깨뜨려버리자" >> $FILE
 git add $FILE
 git commit -m "Add 14th sentence"
 
-echo "두려움은 이제 거둬"
+echo "두려움은 이제 거둬" >> $FILE
 git add $FILE
 git commit -m "Add 15th sentence"
 
-echo "오로지 나를 믿어"
+echo "오로지 나를 믿어" >> $FILE
 git add $FILE
 git commit -m "Add 16th sentence"
 
-echo "지금이 바로 Time to fly"
+echo "지금이 바로 Time to fly" >> $FILE
 git add $FILE
 git commit -m "Add 17th sentence"
 
-echo "두 눈 앞의 끝 사뿐 넘어가"
+echo "두 눈 앞의 끝 사뿐 넘어가" >> $FILE
 git add $FILE
 git commit -m "Add 18th sentence"
 
-echo "한계 밖의 Trip 짜릿하잖아"
+echo "한계 밖의 Trip 짜릿하잖아" >> $FILE
 git add $FILE
 git commit -m "Add 19th sentence"
 
-echo "녹이 슨 심장에 쉼 없이 피는 꿈"
+echo "녹이 슨 심장에 쉼 없이 피는 꿈" >> $FILE
 git add $FILE
 git commit -m "Add 20th sentence"
 
-echo "무모하대도 믿어 난"
+echo "무모하대도 믿어 난" >> $FILE
 git add $FILE
 git commit -m "Add 21th sentence"
